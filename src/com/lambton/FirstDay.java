ba6fb97e21ac626ecd981e170e068fe20da29a0e
ritik jagpal <ritikjagpal@gmail.com>
2019-09-18 20:23:43 -0400

    commit

---
+++ src/com/lambton/FirstDay.java
@@ -0,0 +1,22 @@
+package com.lambton;
+
+public class FirstDay
+{
+    public static void main(String[] args )
+    {
+        System.out.println("Welcome to java programming");
+        int a=10,b=20,c;
+        c=a+b;
+        //display result
+        System.out.println("sum :" +c);
+
+        int x= Integer.MAX_VALUE;
+        System.out.println("X :"+x);
+
+        int y=x+1;
+
+        System.out.println("Y :"+y);
+        
+
+    }
+}
